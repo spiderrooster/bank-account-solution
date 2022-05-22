bab7b8dfe6b7b5c0a9fd05c84af91158394d1ce2
Louis Thorne <louisthorne12@gmail.com>
2022-05-23 00:41:28 +0100

    First inital commit working on stencilling out bank account class requirements

---
+++ bank-account-attempt/src/main/java/BankAccount.java
@@ -0,0 +1,24 @@
+
+public class BankAccount {
+
+    public BankAccount() {
+    }
+
+    public void open() {
+
+    }
+
+    public void close()
+    {
+
+    }
+
+    public double getBalance() {
+
+        return 0;
+    }
+
+    public void deposit(double value) {
+
+    }
+}
